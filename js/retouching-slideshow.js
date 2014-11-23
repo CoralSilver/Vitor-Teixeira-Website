de364ccfe651aec7fb19f3e8f3474f47699ffa98
CoralSilver <coralsilverman@gmail.com>
2014-11-23 18:02:51 -0500

    initial commit

---
+++ js/retouching-slideshow.js
@@ -0,0 +1,82 @@
+$( document ).ready(function() {
+
+		$("#before1 img").on("click", function () {
+    if ($(this).src.match("_before")) {
+        $(this).src = "imgs/retouched/tattoo_after.jpg";
+        $(this).parent().find("p").html("After");
+    } else {
+        $(this).src = "imgs/retouched/tattoo_before.jpg";
+        $(this).parent().find("p").html("Before");
+    }
+});
+
+		$("#before2 img").on("click", function(){
+		    if ($(this).src.match("_before")) {
+		        $(this).src = "imgs/retouched/rings_after.jpg";
+		        $(this).parent().find("p").html("After");
+		    } else {
+		        $(this).src = "imgs/retouched/rings_before.jpg";
+		        $(this).parent().find("p").html("Before");
+		    }
+		});
+
+		$("#before3 img").on("click", function(){
+		    if ($(this).src.match("_before")) {
+		        $(this).src = "imgs/retouched/pistol_after.jpg";
+		        $(this).parent().find("p").html("After");
+		    } else {
+		        $(this).src = "imgs/retouched/pistol_before.jpg";
+		        $(this).parent().find("p").html("Before");
+		    }
+		});
+
+		$("#before4 img").on("click", function(){
+		    if ($(this).src.match("_before")) {
+		        $(this).src = "imgs/retouched/whiskey_after.jpg";
+		        $(this).parent().find("p").html("After");
+		    } else {
+		        $(this).src = "imgs/retouched/whiskey_before.jpg";
+		        $(this).parent().find("p").html("Before");
+		    }
+		});
+
+		$("#before5 img").on("click", function(){
+		    if ($(this).src.match("_before")) {
+		        $(this).src = "imgs/retouched/sushi_after.jpg";
+		        $(this).parent().find("p").html("After");
+		    } else {
+		        $(this).src = "imgs/retouched/sushi_before.jpg";
+		        $(this).parent().find("p").html("Before");
+		    }
+		});
+
+		$("#before6 img").on("click", function(){
+		    if ($(this).src.match("_before")) {
+		        $(this).src = "imgs/retouched/sculpture_after.jpg";
+		        $(this).parent().find("p").html("After");
+		    } else {
+		        $(this).src = "imgs/retouched/sculpture_before.jpg";
+		        $(this).parent().find("p").html("Before");
+		    }
+		});
+
+		$("#before7 img").on("click", function(){
+		    if ($(this).src.match("_before")) {
+		        $(this).src = "imgs/retouched/landscape_after.jpg";
+		        $(this).parent().find("p").html("After");
+		    } else {
+		        $(this).src = "imgs/retouched/landscape_before.jpg";
+		        $(this).parent().find("p").html("Before");
+		    }
+		});
+
+		$("#before8 img").on("click", function(){
+		    if ($(this).src.match("_before")) {
+		        $(this).src = "imgs/retouched/fish_after.jpg";
+		        $(this).parent().find("p").html("After");
+		    } else {
+		        $(this).src = "imgs/retouched/fish_before.jpg";
+		        $(this).parent().find("p").html("Before");
+		    }
+		});
+});
